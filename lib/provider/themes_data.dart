eade9949425dbc81136150ca51edf7e4d8091565
Ali Tahir <alitahir2013@gmail.com>
2024-01-24 03:08:41 +0500

    Themes updated

--- lib/provider/themes_data.dart
+++ lib/provider/themes_data.dart
@@ -2,3 +2,5 @@ import 'package:flutter/material.dart';
 
 // Dark Theme Data
 Color darkThemeScaffold = const Color.fromARGB(255, 34, 36, 49);
+Color darkThemePrimaryFont = Colors.white;
+Color darkThemeSecondaryFont = Colors.white;
