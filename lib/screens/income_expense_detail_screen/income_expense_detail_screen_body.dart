1b01a2fdd8117e8cf98d0817b8fee0140fd28abe
Ali Tahir <alitahir2013@gmail.com>
2024-01-28 23:25:47 +0500

    tooltip color changed

--- lib/screens/income_expense_detail_screen/income_expense_detail_screen_body.dart
+++ lib/screens/income_expense_detail_screen/income_expense_detail_screen_body.dart
@@ -1,8 +1,6 @@
 import 'package:cash_book_expense_tracker/screens/home_screen/appbar.dart';
+import 'package:cash_book_expense_tracker/screens/income_expense_detail_screen/graph.dart';
 import 'package:flutter/material.dart';
-import 'package:fl_chart/fl_chart.dart';
-import '../../provider/themes_data.dart';
-import '../../widgets/gradient_box.dart';
 
 class MyIncomeExpenseDetailScreenBody extends StatefulWidget {
   const MyIncomeExpenseDetailScreenBody({super.key});
@@ -50,100 +48,7 @@ class _MyIncomeExpenseDetailScreenBodyState
                 ),
               ),
             ),
-            Container(
-              margin: EdgeInsets.symmetric(vertical: 20, horizontal: 20),
-              child: Padding(
-                padding: const EdgeInsets.only(right: 10, top: 20),
-                child: SizedBox(
-                  height: 200,
-                  child: Center(
-                    child: LineChart(
-                      LineChartData(
-                          backgroundColor: Colors.transparent,
-                          borderData: FlBorderData(
-                              border: Border(
-                                  // right: BorderSide(color: Colors.black),
-                                  // bottom: BorderSide(color: Colors.black),
-                                  )),
-                          titlesData: FlTitlesData(
-                            rightTitles: AxisTitles(drawBelowEverything: false),
-                            topTitles: AxisTitles(drawBelowEverything: false),
-                          ),
-                          gridData: FlGridData(
-                            checkToShowHorizontalLine: (value) => true,
-                            checkToShowVerticalLine: (value) => false,
-                          ),
-                          lineBarsData: [
-                            LineChartBarData(
-                              belowBarData: BarAreaData(
-                                show: true,
-                                gradient: LinearGradient(
-                                  colors: args[1] == "Income"
-                                      ? [
-                                          ColorTween(
-                                                  begin: incomeLight,
-                                                  end: incomeLight)
-                                              .lerp(1)!
-                                              .withOpacity(0.2),
-                                          ColorTween(
-                                                  begin: incomeLight,
-                                                  end: incomeLight)
-                                              .lerp(1)!
-                                              .withOpacity(0.2),
-                                        ]
-                                      : [
-                                          ColorTween(
-                                                  begin: expenseLight,
-                                                  end: expenseLight)
-                                              .lerp(1)!
-                                              .withOpacity(0.2),
-                                          ColorTween(
-                                                  begin: expenseLight,
-                                                  end: expenseLight)
-                                              .lerp(1)!
-                                              .withOpacity(0.2),
-                                        ],
-                                ),
-                              ),
-                              gradient: LinearGradient(
-                                  colors: args[1] == "Income"
-                                      ? [incomeDark, incomeLight]
-                                      : [expenseDark, expenseLight]),
-                              // dashArray: [1, 2, 3, 4, 5],
-
-                              dotData: FlDotData(
-                                checkToShowDot: (spot, barData) {
-                                  // if (spot.x == 0 && spot.y == 0) {
-                                  return false;
-                                  // } else {
-                                  // return true;
-                                  // }
-                                },
-                              ),
-                              dashArray: [
-                                2,
-                                5,
-                                10,
-                                5,
-                              ],
-                              spots: const [
-                                FlSpot(0, 0),
-                                FlSpot(1, 3),
-                                FlSpot(2, 7),
-                                FlSpot(3, 14),
-                                FlSpot(4, 19),
-                                FlSpot(5, 20),
-                                FlSpot(6, 22),
-                                FlSpot(7, 28),
-                                FlSpot(8, 40),
-                              ],
-                            ),
-                          ]),
-                    ),
-                  ),
-                ),
-              ),
-            )
+            MyIncomeExpenseGraph(args),
           ],
         ),
       ),
